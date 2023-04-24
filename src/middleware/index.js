67778746698410390a52731560c90782b4fdca4b
Arditti93 <alex.arditti@hotmail.com>
2023-04-24 17:26:59 +0100

    add comparePassword

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -24,11 +24,25 @@ const comparePass = async (req, res, next) => {
         // Error handeling if password don't match OR username doesn't exist in the database 
 
         // if they do match, continue to the controller
+        req.user = await User.findOne({where: {username: req.body.username}})      
+
+        if (req.user === null) {
+            throw new Error ("password or username doesn't match")
+        }
+        // let hashedPassword = req.user.password
+        const comparePassword = await bcrypt.compare(req.body.password, req.user.password)
+
+        if(!comparePassword){
+            throw new Error ("password or username doesn't match")
+        } 
+        
+        next()
     } catch (error) {
         res.status(501).json({errorMessage: error.message, error: error})
     }
 }
 
 module.exports = {
-    hashPass
+    hashPass,
+    comparePass
 }
--- src/users/routes.js
+++ src/users/routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express")
 
 const userRouter = Router()
 
-const {registerUser,getAllUsers, updateUser, deleteUser } = require("./controllers") 
-const { hashPass } = require("../middleware")
+const {registerUser,getAllUsers, updateUser, deleteUser, login } = require("./controllers") 
+const { hashPass, comparePass } = require("../middleware")
 
 userRouter.post("/users/register", hashPass, registerUser)
+
+userRouter.post("/users/login", comparePass, login)
 
 userRouter.get("/users/getUsers", getAllUsers)
 
